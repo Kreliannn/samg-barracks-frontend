31b4b59bf05e20c7cca5a7cb9462239a074fb5ae
Kreliannn <krelianquimson@gmail.com>
2025-07-10 16:46:54 +0800

    display data

---
+++ src/app/pages/manager/request/components/orderHistory.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/app/utils/axios";
+import { getIngredientsInterface } from "@/app/types/ingredient.type";
+import { successAlert, errorAlert } from "@/app/utils/alert";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  import { requestInterface } from "@/app/types/request.type";
+  import { Button } from "@/components/ui/button";
+  import { Input } from "@/components/ui/input";
+import useUserStore from "@/app/store/user.store";
+import { confirmAlert } from "@/app/utils/alert";
+import { getRequestInterface } from "@/app/types/request.type";
+
+
+
+export default function OrderHistory({request} : {request : getRequestInterface[]}) {
+
+    const filteredData = request.filter((item) => item.status == "completed" )
+
+
+  return (
+    <div className="h-full w-full bg-stone-100">
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Orders</TableHead>
+                    <TableHead>Total</TableHead>
+                    <TableHead >Status</TableHead>
+                    <TableHead > View orders </TableHead>
+                    <TableHead > Action </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {filteredData.map((item) => (
+                    <TableRow key={item._id}>
+                        <TableCell className="font-medium">{item.date}</TableCell>
+                        <TableCell className="font-medium">{item.request.length}</TableCell>
+                        <TableCell className="font-medium">{item.total}</TableCell>
+                        <TableCell className="font-medium">{item.status}</TableCell>
+                        <TableCell className="font-medium">
+                            <Button> view </Button>
+                        </TableCell>
+                        <TableCell className="font-medium">
+                            <Button> Received </Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </div>
+  );
+}
--- src/app/pages/manager/request/page.tsx
+++ src/app/pages/manager/request/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 import RequestItem from "./components/requestItem";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/app/utils/axios";
+import { getRequestInterface } from "@/app/types/request.type";
+import OrderRequest from "./components/orderRequest";
+import OrderHistory from "./components/orderHistory";
+
 
 export default function Page() {
 
+  const [request, setRequest] = useState<getRequestInterface[]>([])
+ 
+  const { data } = useQuery({
+    queryKey: ["request"],
+    queryFn: () => axiosInstance.get("/request/branch")
+  })
 
+  useEffect(() => {
+      if(data?.data) setRequest(data?.data)
+  }, [data])
+
+  console.log(request)
 
     
   return (
@@ -16,12 +34,12 @@ export default function Page() {
       {/* Second column with 2 rows */}
       <div className="grid grid-rows-2 gap-[1%]">
 
-        <div className="bg-green-100">
-
+        <div className="">
+         <OrderRequest request={request}/>
         </div>
 
-        <div className="bg-yellow-100">
-
+        <div className="">
+          <OrderHistory request={request}/>
         </div>
 
       </div>
