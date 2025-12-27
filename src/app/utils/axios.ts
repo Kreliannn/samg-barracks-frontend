b798e96d2287be0af0d8a292a06ce13a244ffdb6
Kreliannn <krelianquimson@gmail.com>
2025-12-27 11:04:28 +0800

    update server

--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // CHOICES
 const localhost = "http://localhost:5000"
-const live = "https://barracks-backend.onrender.com"
+const live = "https://barracks-server.onrender.com"
 
 const axiosInstance = axios.create({
   baseURL: live,  
