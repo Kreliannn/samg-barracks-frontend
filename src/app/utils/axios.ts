0de765d1e03bfa59653ba5201170e97586aab2b9
Kreliannn <krelianquimson@gmail.com>
2025-12-27 11:24:18 +0800

    update sereever

--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // CHOICES
 const localhost = "http://localhost:5000"
+const localhostTest = "http://localhost:5000"
 const live = "https://barracks-server.onrender.com"
 
 const axiosInstance = axios.create({
